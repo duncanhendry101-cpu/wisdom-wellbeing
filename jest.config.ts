35af7a7d49cdf8b47b663eec8052d46c00e5c9b7
Duncan <duncanhendry101@gmail.com>
2026-06-14 17:16:03 +0200

    Implement test configuration

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,22 @@ const config: Config = {
   testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.module\\.css$": "identity-obj-proxy",
+    "\\.css$": "identity-obj-proxy",
+
+    "^react$": "<rootDir>/node_modules/react",
+    "^react-dom$": "<rootDir>/node_modules/react-dom",
+    "^react-dom/client$": "<rootDir>/node_modules/react-dom/client",
+    "^react-dom/server$": "<rootDir>/node_modules/react-dom/server",
+  },
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        // 🚀 Swap out the base app file for our targeted testing config override
+        tsconfig: "./tsconfig.test.json",
+      },
+    ],
   },
 };
 
