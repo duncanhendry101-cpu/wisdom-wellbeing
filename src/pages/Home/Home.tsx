077d26ba734126300c54259a7fc49c997ff6fe9a
Duncan <duncanhendry101@gmail.com>
2026-06-14 15:30:36 +0200

    Implement home page layout and filter

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,31 @@
-import styles from "./Home.module.css";
-import { ResourceCard } from "@/components/ResourceCard/ResourceCard";
 import type { Resource } from "@/types/resource";
+import styles from "./Home.module.css";
+import { fetchResources } from "@/util/fetchResources";
+import { useEffect, useState } from "react";
+import { ResourceCard } from "@/components/ResourceCard/ResourceCard";
+import { ResourceFilter } from "./ResourceFilter/ResourceFilter";
 
 export function Home() {
-  return <div className={styles.homeContainer}>home</div>;
+  const [resources, setResources] = useState<Resource[]>([]);
+
+  const loadResources = async () => {
+    const data = await fetchResources();
+    setResources(data);
+  };
+
+  useEffect(() => {
+    loadResources();
+  }, []);
+
+  return (
+    <div className={styles.homeContainer}>
+      <ResourceFilter />
+
+      <div className={styles.resourcesContainer}>
+        {resources.map((resource) => (
+          <ResourceCard key={resource.id} data={resource} />
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/pages/Home/ResourceFilter/ResourceFilter.tsx
@@ -0,0 +1,10 @@
+// import { ResourceCard } from "@/components/ResourceCard/ResourceCard";
+import styles from "./ResourceFilter.module.css";
+
+export function ResourceFilter() {
+  return (
+    <div className={styles.filterContainer}>
+      <div className={styles.filterInner}>filter</div>
+    </div>
+  );
+}
