870633f8a2542b5b059715286653c6f345083e5b
Duncan <duncanhendry101@gmail.com>
2026-06-14 15:30:01 +0200

    Set up resource card

--- src/components/ResourceCard/ResourceCard.tsx
+++ src/components/ResourceCard/ResourceCard.tsx
@@ -1,10 +1,32 @@
 import type { Resource } from "@/types/resource";
-import styles from "./Resource.module.css";
+import styles from "./ResourceCard.module.css";
 
 interface ResourceProps {
   data: Resource;
 }
 
 export function ResourceCard({ data }: ResourceProps) {
-  return <div className={styles.resource}>Resource</div>;
+  return (
+    <div className={styles.resourceContainer}>
+      <img
+        className={styles.thumbnail}
+        src={data.thumbnail}
+        width="100%"
+        height="200"
+        alt={data.title}
+      />
+      <h4 className={styles.title}>{data.title}</h4>
+      <p>{data.duration} minutes</p>
+
+      {data?.tags?.length > 0 && (
+        <div className={styles.tagsContainer}>
+          {data.tags.map((tag) => (
+            <span key={tag} className={styles.tag}>
+              {tag}
+            </span>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
