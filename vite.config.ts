c0c977c977b99a09c27053e9a1ac97ae1cb40652
Duncan <duncanhendry101@gmail.com>
2026-06-14 13:06:29 +0200

    Add packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  /* ADD THIS BLOCK RIGHT HERE */
+  optimizeDeps: {
+    include: ["react-router-dom", "react-router", "react", "react-dom"],
+  },
+});
